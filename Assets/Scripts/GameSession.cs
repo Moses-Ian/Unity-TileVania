7c12cc5aec22569500911384adf0a240e670d388
Ian Moses <ian.moses@cognizant.com>
2023-10-05 11:02:10 -0700

    lives and score ui

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
-    [SerializeField] float levelLoadDelay = 5f;
+    [SerializeField] float levelLoadDelay = 3f;
+    [SerializeField] int score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     void Awake()
     {
@@ -22,17 +27,30 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
     public void ProcessPlayerDeath()
     {
         if (playerLives > 1)
         {
             TakeLife();
+            livesText.text = playerLives.ToString();
             StartCoroutine(ReloadLevel());
         }
         else
         {
             StartCoroutine(ResetGameSession());
         }
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
     }
 
     public IEnumerator ReloadLevel()
